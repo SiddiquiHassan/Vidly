15e20b112f1b20c1d18c4461f3d71f9d48c97697
Hassan Siddiqui <sidd_hassan@hotmail.com>
2020-07-19 00:36:08 -0700

    Practice code # 1

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Vidly
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "TestRoute",
+                url: "{controller}/{action}/{year}/{month}",
+                defaults: new { controller = "Movies", action = "ByReleaseDate", year = UrlParameter.Optional, month = UrlParameter.Optional}
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!", Id=90};
+
+            return View(movie);
+            //return Redirect("wwww.google.com");
+            //return RedirectToAction("Index", "Home", new { page = 1, sortby = "name" });
+            //return Content("Using Content !");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return Json(movie,JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content(id.ToString());
+        }
+
+        public ActionResult Index(int? page, string sortBy)
+        {
+            if (!page.HasValue)
+            {
+                page = 567;
+                sortBy = "Drama";
+            }
+            
+            return Content(string.Format("page is {0} and sortBy is {1}", page, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, byte month)
+        {
+            return Content(year + "/" + month);
+        }
+
+    }
+}
